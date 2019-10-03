feaa0a8595ddf447a32958201ff525cbfa4d7749
Michele Proverbio <proch92@gmail.com>
2019-10-03 16:10:13 +0200

    added iss2020Lab submodule. basicrobot deployment gradle file. robotMind

--- basicrobot/basicRobotConfig.pl
+++ basicrobot/basicRobotConfig.pl
@@ -1,4 +1,4 @@
-robot( virtual, "8999" ).     %%the port is the default used by clientWenvObjTcp.kt
+robot( virtual, 8999 ).     %%the port is the default used by clientWenvObjTcp.kt
 %% robot( realmbot, "COM6" ).  %% /dev/ttyUSB0
 %% robot( realnano, "" ).
 
