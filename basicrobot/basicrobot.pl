4385260a20d5b7a48cb8c5ba52092c6ba57944a0
Michele Proverbio <proch92@gmail.com>
2019-10-10 16:24:36 +0200

    lab 10/10

--- basicrobot/basicrobot.pl
+++ basicrobot/basicrobot.pl
@@ -1,5 +1,5 @@
 %====================================================================================
 % basicrobot description   
 %====================================================================================
-context(ctxbasicrobot, "localhost",  "TCP", "8020").
+context(ctxbasicrobot, "10.201.116.57",  "TCP", "8020").
  qactor( basicrobot, ctxbasicrobot, "it.unibo.basicrobot.Basicrobot").
--- robotMind/robotmind.pl
+++ robotMind/robotmind.pl
@@ -2,4 +2,6 @@
 % robotmind description   
 %====================================================================================
 context(ctxmind, "localhost",  "TCP", "8023").
- qactor( basicrobot, ctxmind, "it.unibo.basicrobot.Basicrobot").
+context(ctxbasicrobot, "10.201.116.57",  "TCP", "8020" ).
+ qactor( basicrobot, ctxbasicrobot, "external").
+  qactor( robotmind, ctxmind, "it.unibo.robotmind.Robotmind").
