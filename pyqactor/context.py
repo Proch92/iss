afdf222d20b144a6a418111f2bebb62b27ed8b78
Michele Proverbio <proch92@gmail.com>
2019-12-05 12:31:32 +0100

    working on mqtt

--- pyqactor/context.py
+++ pyqactor/context.py
@@ -2,20 +2,39 @@ from fsmactor import FSMactor
 import pyqak
 import asyncio
 import messages
-import socket
+import paho.mqtt.client as mqtt
 
 
 class Context(object):
-    def __init__(self, name, host, port):
+    def __init__(self, name, host, port, mqtt=None, mqtt_port=1883):
+        self.mqtt = mqtt
+        self.mqtt_port = mqtt_port
         self.name = name
         self.host = host
         self.port = port
         self.actors = {}
         pyqak.add_context(self)
 
+    def on_mqtt_connect(self, client, userdata, flags, rc):
+        print(f"Connected with result code {rc}")
+        client.subscribe("unibo/qak/events")
+        for actor in self.actors:
+            client.subscribe(f"unibo/qak/{actor}")
+
+    def on_mqtt_message(self, client, userdata, msg):
+        msg = messages.parse_message(msg.payload)
+        for ctx in pyqak.contexts:
+            if msg._to in ctx.actors:
+                await ctx.send_message(msg)
+
     async def init(self):
         self.server = await asyncio.start_server(self.handle_conn, self.host, self.port)
         await self.server.start_serving()
+        if self.mqtt:
+            client = mqtt.Client()
+            client.on_connect = self.on_mqtt_connect
+            client.on_message = self.on_mqtt_message
+            client.connect(self.mqtt, self.mqtt_port, 60)
 
     async def handle_conn(self, reader, writer):
         data = await reader.readline()
@@ -87,13 +106,12 @@ class ExternalContext(Context):
         self.actors.append(name)
 
     async def send_message(self, msg):
-        encoded = messages.natali_encode(msg)
-        encoded = str(encoded) + '\n'
-        encoded = encoded.encode()
-        self.writer.write(encoded)
-        await self.writer.drain()
+        await self.tcp_send(msg)
 
     async def send_event(self, msg):
+        await self.tcp_send(msg)
+
+    async def tcp_send(self, msg):
         encoded = messages.natali_encode(msg)
         encoded = str(encoded) + '\n'
         encoded = encoded.encode()
